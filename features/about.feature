890be403369cf9740c054ee5642193cf15968848
eric.king <jinqiqiqi@gmail.com>
2017-03-17 13:10:17 +0800

    add user curd;

--- features/about.feature
+++ features/about.feature
@@ -24,3 +24,23 @@ Feature: about page
     Scenario: Visiting about page for non existing user
     Given I am on "/about/jacks"
     Then I should see "404 Not Found"
+
+
+    @javascript
+    Scenario: create a new user::
+    Given I am on "/user"
+    When I click on "Create a new user"
+    And I fill the  "Name" with "JinQi"
+    And I fill the "Bio" with "This is a tough guy"
+    And I fill the "Email" with "jinqi@gmail.com"
+    And I press "Create"
+    Then I should see "Back to the list"
+
+
+    @javascript
+    Scenario: Delete a user
+    Given I am on "user/"
+    And I click on "show"
+    And I press delete
+    Then I should see "Users list"
+
--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,5 +1,6 @@
 <?php
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\MinkExtension\Context\MinkContext;
 use Behat\Behat\Context\SnippetAcceptingContext;
 
@@ -9,7 +10,7 @@ class FeatureContext extends MinkContext implements SnippetAcceptingContext
     /**
      * @When I press more
      */
-    public function iPressMoref()
+    public function iPressMore()
     {
         // throw new PendingException();
         $this->getSession()
@@ -28,4 +29,50 @@ class FeatureContext extends MinkContext implements SnippetAcceptingContext
             ->findButton("back")
             ->press();
     }
+
+    /**
+     * @When I click on :arg1
+     */
+    public function iClickOn($arg1)
+    {
+        // throw new PendingException();
+        $this->getSession()->getPage()->clickLink($arg1);
+    }
+
+    /**
+     * @When I fill the  :arg1 with :arg2
+     */
+    public function iFillTheWith($arg1, $arg2)
+    {
+        // throw new PendingException();
+        $this->getSession()
+            ->getPage()
+            ->fillField($arg1, $arg2);
+    }
+
+    /**
+     * @When I fill the :arg1 with :arg2
+     */
+    public function iFillTheWith2($arg1, $arg2)
+    {
+        // throw new PendingException();
+        $this->getSession()
+            ->getPage()
+            ->fillField($arg1, $arg2);
+    }
+
+
+
+    /**
+     * @Given I press delete
+     */
+    public function iPressDelete()
+    {
+        // throw new PendingException();
+        $this->getSession()
+            ->getPage()
+            ->findButton("Delete")
+            ->press();
+    }
+
 }
