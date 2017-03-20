7ac01fb8130132c78d631c3a6f7a0cba2d30a464
kinch <jinqiqiqi@gmail.com>
2017-03-20 21:03:39 +0800

    activate the fixtures-bundle, and create the fixture file for user.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,11 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
+            // third party bundles
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+
+            // custom bundles
             new AppBundle\AppBundle(),
         );
 
