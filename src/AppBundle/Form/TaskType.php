03828fac4bbf831864e5567d268b134c41a18915
eric.king <jinqiqiqi@gmail.com>
2017-03-21 12:11:04 +0800

    generate cruds for workspace, project, task

---
+++ src/AppBundle/Form/TaskType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('title')->add('description')->add('dueDate')->add('attachment')
+            ->add('project', 'entity', [
+                'class' => 'AppBundle\Entity\Project',
+                'property' => 'title'
+            ])
+            ->add('user', 'entity', [
+                'class' => 'AppBundle\Entity\User',
+                'property' => 'username'
+            ]);
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Task'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_task';
+    }
+
+
+}
